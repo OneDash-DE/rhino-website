ef3cc84009ac2bb711f6accb13f692e4714a4e3e
D074015 <jonathan.busshoff@sap.com>
2023-02-21 20:52:37 +0100

    new header image implemented

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,14 +10,25 @@ interface HeaderProps {
 }
 
 export const Header = ({ socialMediaLinks, homepage }: HeaderProps) => {
-	const logoUrl = homepage.attributes?.logoImage?.data?.attributes?.url;
+	const logo = homepage.attributes?.logoImage?.data?.attributes;
 	return (
 		<header className={styles.header}>
 			<SocialMediaIcons socialMediaLinks={socialMediaLinks} />
-			<div>
-				<h1 id="home" className={styles.headerText}>
-					The Rhino
-				</h1>
+			<div style={logo?.url ? { padding: "0.5rem 0" } : {}}>
+				{logo?.url ? (
+					<div
+						style={{
+							position: "relative",
+							width: "clamp(350px, 60vw, 850px)",
+							paddingBottom: `${((logo.height ?? 0) / (logo.width ?? 0)) * 100}%`,
+						}}>
+						<Image fill src={logo.url} alt="The Rhino" />
+					</div>
+				) : (
+					<h1 id="home" className={styles.headerText}>
+						The Rhino
+					</h1>
+				)}
 			</div>
 			<div />
 		</header>
